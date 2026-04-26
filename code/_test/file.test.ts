5766fa7d3542ea55a57a36ef248cb52085c2052e
lignx <xzzulz@gmail.com>
2026-04-26 06:24:40 -0400

    File tests fixes
    test files where moved to new dir.
    Updates file paths.
    And small fixes.

--- code/_test/file.test.ts
+++ code/_test/file.test.ts
@@ -5,12 +5,14 @@ import { get_file_timestamp, copy_newer } from "../file/file-time.ts";
 
 
 //
-// before all, clear content of test/file-test/dist
+// before all, clear content of test-files/step and test-files/dist
 //
 Deno.test.beforeAll(async () => {
 
+    // Delete all content of test-files/step and test-files/dist
     try {
-        await Deno.remove("test-files/file-test/dist", { recursive: true });
+        await Deno.remove("test-files/step", { recursive: true });
+        await Deno.remove("test-files/dist", { recursive: true });
     } catch (error) {
         if (error instanceof Deno.errors.NotFound) {
             // Directory doesn't exist, which is fine
@@ -18,7 +20,9 @@ Deno.test.beforeAll(async () => {
         }
         throw error; // Re-throw other errors
     }
-    await Deno.mkdir("test-files/file-test/dist");
+
+    await Deno.mkdir("test-files/step");
+    await Deno.mkdir("test-files/dist");
 });
 
 
@@ -26,7 +30,7 @@ Deno.test.beforeAll(async () => {
 // Test get_file_timestamp
 //
 Deno.test("get_file_timestamp", async () => {
-    let timestamp = await get_file_timestamp("test-files/file-test/code/test-file.txt");
+    let timestamp = await get_file_timestamp("test-files/code/test-file.txt");
     assert(timestamp > 0);
 
     let date_string = new Date(timestamp).toDateString();
@@ -42,14 +46,14 @@ Deno.test("get_file_timestamp", async () => {
 // To verify that it copies only newer files.
 //
 Deno.test("copy_newer", async () => {
-    await copy_newer("test-files/file-test/code", "test-files/file-test/dist");
+    await copy_newer("test-files/code", "test-files/step", { files: [], dirs: [] });
 
-    // assert that dist/test-file.txt exists
-    assert(await Deno.stat("test-files/file-test/dist/test-file.txt"));
+    // assert that step/test-file.txt exists
+    assert(await Deno.stat("test-files/step/test-file.txt"));
 
-    // assert that dist/test-file.txt content
-    let code_content = await Deno.readTextFile("test-files/file-test/code/test-file.txt");
-    let dist_content = await Deno.readTextFile("test-files/file-test/dist/test-file.txt");
-    assertEquals(code_content, dist_content);
+    // assert that is newer than the original
+    let step_timestamp = await get_file_timestamp("test-files/step/test-file.txt");
+    let code_timestamp = await get_file_timestamp("test-files/code/test-file.txt");
+    assert(step_timestamp > code_timestamp);
 
 })
--- workflow.md
+++ workflow.md
@@ -1,9 +1,10 @@
 # Workflow
 
 ## Tests
-
+Run tests with deno "test" command.
+Tests need dir permissions:
 ```bash
-deno test -R=./test/ -W=./test/
+deno test -R=./test-files/ -W=./test-files/
 ```
 
 ## Build
